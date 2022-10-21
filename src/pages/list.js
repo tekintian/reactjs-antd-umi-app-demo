6c77eb285101afeb0b808d351e8e625ab6ab0156
Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>
2022-10-21 13:04:33 +0800

    打开页面不触发，点击时触发的函数 shouldComponentUpdate
    Signed-off-by: Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>

--- src/pages/list.js
+++ src/pages/list.js
@@ -8,7 +8,13 @@ class List extends React.Component {
             maxNum: 5
         }
     }
-
+    shouldComponentUpdate(nextProps, nextState) {
+    // 每当this.props或this.state有变化，在render方法执行之前，就会调用这个方法。
+    // 该方法返回一个布尔值，表示是否应该继续执行render方法，即如果返回false，UI 就不会更新，默认返回true。
+    // 组件挂载时，render方法的第一次执行，不会调用这个方法。
+        console.log("shouldComponentUpdate()");
+        return true;
+    }
     render(){
         return (<div>
             <ul>
