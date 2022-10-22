7634b20e46be83c55d392a814c80d9e56af560ee
Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>
2022-10-22 10:47:33 +0800

    获取URL参数示例
    Signed-off-by: Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,28 @@ import React from "react";
 import HelloWorld from "../component/HelloWorld";
 
 class index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //状态数据
+    this.state = {
+      name: "Tekin",
+    };
+  }
+  componentDidMount() {
+    //===获取URL中的参数 start ====
+    console.log(this.props.location.query);
+    let qname = this.props.location.query.name;
+    if (qname) {
+      //如果参数不为空，则使用setState修改状态数据
+      this.setState({
+        name: qname,
+      });
+    }
+    //====end====
+  }
   render() {
-    return <HelloWorld name="Tekin">My first react app</HelloWorld>; //使用helloworld组件
+    return <HelloWorld name={this.state.name}>My first react app</HelloWorld>; //使用helloworld组件
   }
 }
 export default index;
