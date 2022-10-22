6ad428440d4954b5167844771fc7123f0c12b53c
Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>
2022-10-22 12:47:52 +0800

    https://3x.ant.design/components/menu-cn/
    Signed-off-by: Tekin Tian <Tekin@MacdeMacBook-Pro-352.local>

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,81 +1,90 @@
 import React from "react";
-import { Layout, Menu, Icon } from "antd";
+import { Layout, Menu, Icon, Button } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 const { SubMenu } = Menu;
 
 class BaseLayout extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      collapsed: false,
-    };
   }
+  state = {
+    collapsed: false,
+  };
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
 
   render() {
     return (
       <Layout>
         <Sider width={256} style={{ minHeight: "100vh", color: "white" }}>
-          <div
-            style={{
-              height: "32px",
-              background: "rgba(255,255,255,.2)",
-              margin: "16px",
-            }}
-          />
-          <Menu
-            defaultSelectedKeys={["2"]}
-            defaultOpenKeys={["sub1"]}
-            mode="inline"
-            theme="dark"
-            inlineCollapsed={this.state.collapsed}
-          >
-            <Menu.Item key="1">
-              <Icon type="pie‐chart" />
-              <span>Option 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="desktop" />
-              <span>Option 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="inbox" />
-              <span>Option 3</span>{" "}
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="mail" />
-                  <span>Navigation One</span>
-                </span>
-              }
+          <div style={{ width: 256 }}>
+            <Button
+              type="primary"
+              onClick={this.toggleCollapsed}
+              style={{ marginBottom: 16 }}
             >
-              <Menu.Item key="5">Option 5</Menu.Item>
-              <Menu.Item key="6">Option 6</Menu.Item>
-              <Menu.Item key="7">Option 7</Menu.Item>
-              <Menu.Item key="8">Option 8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="appstore" />
-                  <span>Navigation Two</span>
-                </span>
-              }
+              <Icon type={this.state.collapsed ? "menu-unfold" : "menu-fold"} />
+            </Button>
+            <Menu
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              mode="inline"
+              theme="dark"
+              inlineCollapsed={this.state.collapsed}
             >
-              <Menu.Item key="9">Option 9</Menu.Item>
-              <Menu.Item key="10">Option 10</Menu.Item>
-              <SubMenu key="sub3" title="Submenu">
-                <Menu.Item key="11">Option 11</Menu.Item>
-                <Menu.Item key="12">Option 12</Menu.Item>
+              <Menu.Item key="1">
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="desktop" />
+                <span>Option 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="inbox" />
+                <span>Option 3</span>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={
+                  <span>
+                    <Icon type="mail" />
+                    <span>Navigation One</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="5">Option 5</Menu.Item>
+                <Menu.Item key="6">Option 6</Menu.Item>
+                <Menu.Item key="7">Option 7</Menu.Item>
+                <Menu.Item key="8">Option 8</Menu.Item>
               </SubMenu>
-            </SubMenu>
-          </Menu>
+              <SubMenu
+                key="sub2"
+                title={
+                  <span>
+                    <Icon type="appstore" />
+                    <span>Navigation Two</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="9">Option 9</Menu.Item>
+                <Menu.Item key="10">Option 10</Menu.Item>
+                <SubMenu key="sub3" title="Submenu">
+                  <Menu.Item key="11">Option 11</Menu.Item>
+                  <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+              </SubMenu>
+            </Menu>
+          </div>
         </Sider>
         <Layout>
           <Header
-            style={{ background: "#fff", textAlign: "center", padding: 0 }}>
+            style={{ background: "#fff", textAlign: "center", padding: 0 }}
+          >
             Header
           </Header>
           <Content style={{ margin: "24px 16px 0" }}>
